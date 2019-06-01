959661758c07c4529b0ae9dde29dca6169693346
PhuongDuy <kudophuongduy@gmail.com>
2019-06-01 12:19:37 +0700

    fix delete

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,9 @@ Route::get('logout','PageController@getLogout');
 Route::get('location-management/{id}','PageController@getLocationManagement')->middleware('pageLogin');
 Route::post('location-management/{id}','PageController@postUpdateLocationManagement')->middleware('pageLogin');
 Route::post('location-management/add/{id}','PageController@postAddFood')->middleware('pageLogin');
+Route::get('location-management/delete/{id1}/{id}','PageController@DeleteFood')->middleware('pageLogin');
 Route::get('header',function(){
+
 	return view('layout.header');
 });
 Route::get('profile/{id}','PageController@getProfile')->middleware('pageLogin');
